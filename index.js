3fa9d384edd111930595cd01c2f00b4b7f775294
ibrahima <lodiajames@yahoo.com>
2024-03-29 20:48:54 -0400

    third commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
-
+const cors = require('cors')
+app.use(cors())
 
 app.use(express.json())
 
@@ -71,6 +72,6 @@ app.delete('/api/notes/:id', (request, response)=>{
 
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
